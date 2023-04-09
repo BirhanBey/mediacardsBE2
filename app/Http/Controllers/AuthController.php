aeaa3e52cb39db246aaf008124bfdd937bfab423
Birhan Yorukoglu <birhanyorukoglu@gmail.com>
2023-04-09 21:30:40 +0200

    Users url is ready

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -32,6 +32,9 @@ class AuthController extends Controller
         return response($response, 201);
     }
 
+    /**
+     * Login
+     */
     public function login(Request $request)
     {
         $fields = $request->validate([
@@ -59,6 +62,148 @@ class AuthController extends Controller
         return response($response, 201);
     }
 
+
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $users = User::with('urls')->get()->map(function ($user) {
+            $urls = $user->urls->map(function ($url) {
+                return [
+                    'id' => $url->id,
+                    'name' => $url->name,
+                    'link' => $url->link,
+                    'active' => $url->isActive,
+                ];
+            });
+            return [
+                'id_user' => $user->id,
+                'userName' => $user->userName,
+                'email' => $user->email,
+                'img' => $user->img,
+                'description' => $user->description,
+                'created_at' => $user->created_at,
+                'updated_at' => $user->updated_at,
+                'url' => $urls,
+            ];
+        });
+
+        return response()->json($users);
+    }
+    /**
+     * Display the specified resource.
+     */
+    public function show($id)
+    {
+        $user = User::with('urls')->findOrFail($id);
+        $urls = $user->urls->map(function ($url) {
+            return [
+                'id' => $url->id,
+                'name' => $url->name,
+                'link' => $url->link,
+                'active' => $url->isActive,
+            ];
+        });
+        return response()->json([
+            'id_account' => $user->id,
+            'userName' => $user->userName,
+            'email' => $user->email,
+            'img' => $user->img,
+            'description' => $user->description,
+            'created_at' => $user->created_at,
+            'updated_at' => $user->updated_at,
+            'url' => $urls,
+        ]);
+    }
+
+    /**
+     * search for a name
+     */
+    public function search(string $name)
+    {
+        return User::where('userName', 'like', '%' . $name . '%')->get();
+    }
+
+    /**
+     * add new url.
+     */
+    public function url_post(Request $request, $id)
+    {
+        $name = $request->input('name');
+        $link = $request->input('link');
+        $isActive = $request->input('active');
+
+        if (!$name || !$link || !$isActive) {
+            return response()->json([
+                'message' => 'Required parameters missing',
+            ], 400);
+        }
+
+        $user = User::findOrFail($id);
+        $user->urls()->create([
+            "name" => $name,
+            "link" => $link,
+            "isActive" => $isActive
+        ]);
+
+        return response()->json([
+            'message' => 'New URL added successfully',
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $user = User::findOrFail($id);
+        $user->update($request->all());
+        return $user;
+    }
+
+    /**
+     * Update the specified url in user.
+     */
+    public function url_update(Request $request, string $id, string $url_id)
+    {
+        $user = User::findOrFail($id);
+        $url = $user->urls()->find($url_id);
+
+        if (!$url) {
+            return response()->json(['message' => 'URL not found'], 404);
+        }
+
+        $url->update($request->all());
+        return $url;
+    }
+
+    /**
+     * Delete the specified url in user.
+     */
+    public function url_destroy($id, $url_id)
+    {
+        $user = User::findOrFail($id);
+        $url = $user->urls()->find($url_id);
+        if (!$url) {
+            return response()->json(['message' => 'Url not found'], 404);
+        }
+        $url->delete();
+        return response()->json(['message' => 'Url deleted successfully'], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        return User::destroy($id);
+    }
+
+    /**
+     * logout 
+     */
     public function logout(Request $request)
     {
         $request->user()->currentAccessToken()->delete();
--- routes/api.php
+++ routes/api.php
@@ -6,34 +6,35 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 // public routes
-// get all records
-Route::get('/accounts', [AccountController::class, 'index']);
-// get specified account
-Route::get('/accounts/{id}', [AccountController::class, 'show']);
-// search account by name
-Route::get('/accounts/search/{name}', [AccountController::class, 'search']);
+
 // new user registeration
 Route::post('/register', [AuthController::class, 'register']);
 // user login and token creation
 Route::post('/login', [AuthController::class, 'login']);
+// get all users
+Route::get('/users', [AuthController::class, 'index']);
+// get specified user
+Route::get('/users/{id}', [AuthController::class, 'show']);
+// search user by name
+Route::get('/users/search/{name}', [AuthController::class, 'search']);
 
 // protected routes
 Route::group(['middleware' => ['auth:sanctum']], function () {
-    // sent a new record to accounts table
-    Route::post('/accounts', [AccountController::class, 'store']);
-    // change the informations of accounts table by id
-    Route::put('/accounts/{id}', [AccountController::class, 'update']);
-    // delete item from accounts table
-    Route::delete('/accounts/{id}', [AccountController::class, 'destroy']);
     // create new url list
-    Route::post('/accounts/{id}', [AccountController::class, 'url_post']);
+    Route::post('/users/{id}', [AuthController::class, 'url_post']);
+    // change the informations of user by id
+    Route::put('/users/{id}', [AuthController::class, 'update']);
     // change the informations of url table by id
-    Route::put('/accounts/{id}/urls/{url_id}', [AccountController::class, 'url_update']);
+    Route::put('/users/{id}/urls/{url_id}', [AuthController::class, 'url_update']);
     // delete item from url table
-    Route::delete('/accounts/{id}/urls/{url_id}', [AccountController::class, 'url_destroy']);
+    Route::delete('/users/{id}/urls/{url_id}', [AuthController::class, 'url_destroy']);
+    // delete item from users table
+    Route::delete('/users/{id}', [AuthController::class, 'destroy']);
     // user logout
     Route::post('/logout', [AuthController::class, 'logout']);
-
+    
+    // // sent a new record to accounts table
+    // Route::post('/accounts', [AccountController::class, 'store']);
 });
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
